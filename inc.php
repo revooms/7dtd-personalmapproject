49e76444a5caf9fc3e1f35595851267b50ce1093
revooms <iko@mailbox.org>
2015-08-12 14:51:07 +0200

    PHP removal
    Removed .php files

--- inc.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$version = "0.2";
-$laststarted = file_get_contents('laststarted'); // datetime
-$lastgenerated = file_get_contents('lastgenerated'); // datetime
-$title = sprintf("7dtd map v%g - Generated: %s -  %s", $version, $laststarted, $lastgenerated);
-?>
